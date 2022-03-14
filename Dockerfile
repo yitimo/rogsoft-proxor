203972c17a25e5ef7a4a9616b1b1aef8a42a0740
yitimo <admin@yitimo.com>
2022-03-14 19:49:36 +0800

    ADD

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build-env
+
+# TODO: 用于调试 只复现基础的 asp.net core 环境, 并提供 iptables
--- build.sh
+++ build.sh
@@ -1,10 +1,28 @@
 #!/bin/sh
 
+set -e
+
 MODULE=Proxor
 VERSION=`cat ./version|sed -n 1p`
 TITLE=Proxor
 DESCRIPTION=Proxor
 HOME_URL=Module_proxor.asp
+
+echo "[Proxor]前端打包"
+
+cd ./app
+npm run build
+cd ..
+
+rm -rf ./proxor/webs
+cp -r ./app/build ./proxor/webs
+
+echo "[Proxor]前端打包完成"
+
+sleep 1
+
+echo "[Proxor]开发中..."
+exit 1
 
 # TODO: 将 ./proxor 目录打包为 .tar.gz 包
 # TODO: 更新 ./config.json.js 和 ./version 文件, 这两个文件供远程请求访问 用来和本地版本对比实现版本更新
