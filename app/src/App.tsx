985dcfc82e834738bbfc10c07ebc67c47fc3fc39
yitimo <admin@yitimo.com>
2022-03-28 17:54:08 +0800

    UPDATE

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,25 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
+
+import PageHome from './pages/Home'
+import PageHelp from './pages/Help'
+import PageNotFound from './pages/NotFound'
+
+import './App.scss';
+
+const pages = [
+  { name: 'home', title: '首页', component: PageHome, path: '/' },
+  { name: 'help', title: '帮助', component: PageHelp, path: '/help' },
+  { name: 'not-found', title: '', component: PageNotFound, path: '*' },
+]
 
 function App() {
+  const location = useLocation()
+  const navigate = useNavigate()
+  const handleNavigate = (path: string) => {
+    navigate(path, { replace: true })
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="app-header flex">
+        <h1 className="title" onClick={() => handleNavigate('/')}>Proxor</h1>
+        <ul className="navigations flex-1">
+          {
+            pages
+              .filter((e) => ['home', 'not-found'].indexOf(e.name) < 0)
+              .map((e) => (
+                <li
+                  key={e.name}
+                  className={`navigations-item ${e.path === location.pathname ? 'active' : ''}`}
+                  onClick={() => handleNavigate(e.path)}
+                >{e.title}</li>
+              ))
+          }
+        </ul>
       </header>
-    </div>
+      <section className="app-body flex-1">
+        <Routes>
+          {
+            pages.map((e) => (<Route key={e.name} path={e.path} element={<e.component />} />))
+          }
+        </Routes>
+      </section>
+      <footer className="app-footer">
+        Copyright@yitimo.2022
+      </footer>
+    </>
   );
 }
 
